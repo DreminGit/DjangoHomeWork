30f6e411dd4e644fc708ec3ea7731d0fcdd9b017
DreminGit <dremin.da24108292@gmail.com>
2025-01-12 23:43:40 +0300

    Commit #15 HW 22.1

--- catalog/models.py
+++ catalog/models.py
@@ -29,6 +29,40 @@ class Product(models.Model):
     )
     description = models.CharField(
         max_length=150, verbose_name="описание", help_text="опишите продукта"
+    )from datetime import datetime
+
+from django.db import models
+
+
+class Category(models.Model):
+    '''Создаем модель категории товаров'''
+    name = models.CharField(
+        max_length=50,
+        verbose_name="наименование",
+        help_text="введите наименование категории",
+    )
+    description = models.CharField(
+        max_length=200, verbose_name="описание", help_text="введите описание категории"
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Product(models.Model):
+    '''Модель продукта'''
+    name = models.CharField(
+        max_length=50,
+        verbose_name="наименование продукта",
+        help_text="введите наименование продукта",
+    )
+    description = models.CharField(
+        max_length=150, verbose_name="описание", help_text="опишите продукта"
     )
     image = models.ImageField(
         upload_to="products/",
@@ -48,8 +82,11 @@ class Product(models.Model):
         verbose_name="стоимость", help_text="стоимость продукта"
     )
     created_at = models.DateField(default=datetime.now, verbose_name="дата создания")
-    updated_at = models.DateField(default=datetime.now,verbose_name="дата редактирования")
-    views_counter = models.PositiveIntegerField(default=0,verbose_name='просмотры')
+    updated_at = models.DateField(default=datetime.now, verbose_name="дата редактирования")
+    views_counter = models.PositiveIntegerField(default=0, verbose_name='просмотры')
+
+    def get_active_version(self):
+        return self.versions.filter(is_current=True).first()
 
     class Meta:
         verbose_name = "Продукт"
@@ -58,3 +95,46 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+
+class Version(models.Model):
+    '''Модель версии продукта'''
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        verbose_name="Продукт",
+        help_text="Выберите продукт, к которому относится версия",
+        related_name="versions"
+    )
+    version_number = models.SmallIntegerField(
+        unique=True,
+        verbose_name="Номер версии",
+        help_text="Введите номер версии"
+    )
+    version_name = models.CharField(
+        max_length=100,
+        verbose_name="Название версии",
+        help_text="Введите название версии"
+    )
+    is_current = models.BooleanField(
+        default=False,
+        verbose_name="Текущая версия",
+        help_text="Установите, если это текущая версия продукта"
+    )
+
+    def save(self, *args, **kwargs):
+        # Если номер версии не установлен, увеличиваем его
+        if not self.version_number:
+            max_version = Version.objects.filter(product=self.product).aggregate(models.Max('version_number'))[
+                'version_number__max']
+            self.version_number = max_version + 1 if max_version is not None else 1
+
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Версия"
+        verbose_name_plural = "Версии"
+        ordering = ["-is_current", "version_number"]
+
+    def __str__(self):
+        return f"{self.version_name} (Версия {self.version_number})"
